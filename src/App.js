76e7a84d00ac3f12cfc7c155b778a16d78b1de2c
Xander Hathaway <xander.hathaway@30194-MBPR.hsd1.il.comcast.net>
2020-06-11 21:54:33 -0500

    added yml file and simple page edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          COVID-19 Fact Checking
         </p>
         <a
           className="App-link"
