1204338027b92a9ef404e11e99eea24c00dc5eea
sartaj10 <ssbaveja@es.net>
2020-06-24 22:00:15 -0700

    Pass claim from home to prediction page

--- src/components/Predict.js
+++ src/components/Predict.js
@@ -3,9 +3,11 @@ import { withRouter } from "react-router-dom";
 
 class Predict extends React.Component {
     render() {
+        console.log("Predict this.props ", this.props);
         return (
             <div>
                 <h2>Prediction</h2>
+                <h3>Claim is {this.props.location.state.claim}</h3>
             </div>
         );
     }
